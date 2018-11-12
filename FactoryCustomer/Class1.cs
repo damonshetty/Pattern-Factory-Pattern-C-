16daba463511c8341b993d8f30b92a4c23c90fd9
Damon Shetty <damon_53@hotmail.com>
2018-11-12 12:13:25 +0000

    First commit

---
+++ FactoryCustomer/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+
+namespace FactoryCustomer
+{
+    public static class Factory
+    {
+        public static CustomerBase Create(string TypeCust)
+        {
+            if(TypeCust == "Customer")
+            {
+                return new Customer();
+            }
+            else
+            {
+                return new Lead();
+            }
+        }
+
+    }
+}
---
+++ WinformCustomer/FrmCustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer;
+using FactoryCustomer;
+
+namespace WinformCustomer
+{
+    public class FrmCustomer
+    {
+        private CustomerBase cust = null;
+
+        public FrmCustomer()
+        {
+
+        }
+
+    }
+}
